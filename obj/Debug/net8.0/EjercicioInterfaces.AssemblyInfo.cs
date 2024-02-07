7acd6bbfe5924aa3ce4ea1925930db14392c45a0
cae1814 <cae1814@gmail.com>
2024-02-06 20:06:14 -0600

    actu

--- obj/Debug/net8.0/EjercicioInterfaces.AssemblyInfo.cs
+++ obj/Debug/net8.0/EjercicioInterfaces.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioInterfaces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ae444bcb1349a1f8d0299f615e13c0d797e050")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioInterfaces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioInterfaces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
